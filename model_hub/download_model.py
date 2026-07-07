bbab28e0b88a3bdd92e56b1c793697e4fae65bb4
ZYT <1773096519@qq.com>
2026-07-07 11:13:26 +0800

    Initial commit

---
+++ model_hub/download_model.py
@@ -0,0 +1,11 @@
+from huggingface_hub import snapshot_download
+
+model_id = "gradientai/Llama-3-8B-Instruct-Gradient-1048k"
+local_dir = "/data/lzg/zyt/models/Llama-3-8B-Instruct-Gradient-1048k"
+
+snapshot_download(
+    repo_id=model_id,
+    local_dir=local_dir,
+    local_dir_use_symlinks=False,
+    resume_download=True,
+)
